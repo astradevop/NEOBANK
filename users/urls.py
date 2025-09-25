02c4717af8ceca3788b3488e568d70e0a7cc45df
astradevop <astra.devop@gmail.com>
2025-09-25 07:10:02 +0530

    first commit

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from users import views
+app_name = "users"
+urlpatterns = [
+    path("signup/",views.signup,name="signup"),
+    path("login/",views.login,name="login"),
+]
